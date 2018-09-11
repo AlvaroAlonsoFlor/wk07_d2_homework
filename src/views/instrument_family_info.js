04f3ae2c2c98cb54a12f56d639e6dc945fba921c
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-11 20:05:56 +0100

    Finish MVP

--- src/views/instrument_family_info.js
+++ src/views/instrument_family_info.js
@@ -6,10 +6,31 @@ const InstrumentFamilyInfo = function (container) {
 
 InstrumentFamilyInfo.prototype.bindEvents = function () {
   PubSub.subscribe('InstrumentFamilies:family-info', (event) => {
-    const info = event.detail
-    console.log(event.detail);
+    const info = event.detail;
+    this.renderInfo(info);
 
   })
 };
+
+InstrumentFamilyInfo.prototype.renderInfo = function (info) {
+  console.log(info.name);
+  //title
+  const title = document.createElement('h2')
+  title.textContent = info.name;
+  this.container.appendChild(title);
+
+  //description
+  const description = document.createElement('h3')
+  description.textContent = info.description;
+  this.container.appendChild(description);
+
+  //
+
+};
+
+// const infoParagraph = document.createElement('p');
+//   infoParagraph.textContent = `The ${animal.species}, of class '${animal.class}', has a maximum speed of ${animal.maxSpeed} km/h.`;
+//   this.container.innerHTML = '';
+//   this.container.appendChild(infoParagraph);
 
 module.exports = InstrumentFamilyInfo;
